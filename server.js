056a57b0b8fee8a64bacfde96999bfbf90e553e3
Glitch (ozzym1096-project-urlshortener) <none>
2019-04-27 04:11:06 +0000

    Complete(?) Project

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+const dns = require('dns');
 
 var cors = require('cors');
 
@@ -20,6 +21,8 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -27,10 +30,88 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// MondoDB Schema and Model
+let urlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: Number
+  }
+});
+
+let urlModel = mongoose.model("URL Pair", urlSchema);
+
+// Handle API route
+app.post("/api/shorturl/new", function (req, res) {
+  let userUrl = req.body.url;
+  let myRegex = /^https?:\/\//;
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // First test if url points to a valid server
+  dns.lookup(userUrl.replace(myRegex, ""), (err) => {
+    if (err) return res.json({
+      error: "invalid URL"
+    });
+    
+    // If simple test passes, look for long url in DB
+    urlModel.findOne({original_url: userUrl}, (err, pair) => {
+      if (err) return console.error(err);
+      
+      // If not found, create a new entry in DB
+      if (pair === null) {
+        // Search for entries in DB to get short_url
+        return urlModel.find()
+          .select("short_url")
+          // Sort descending to get greatest number first
+          .sort("-short_url")
+          .exec((err, docs) => {
+            // If this is the first item in the DB
+            if (docs.length === 0) {
+              let newPair = new urlModel({
+                original_url: userUrl,
+                short_url: 1
+              });
+              
+              return newPair.save((err, thisDoc) => {
+                if (err) return console.error(err);
+                res.json({
+                  original_url: thisDoc.original_url,
+                  short_url: thisDoc.short_url
+                });
+              });          
+            }
+            
+            let newPair = new urlModel({
+              original_url: userUrl,
+              short_url: docs[0].short_url + 1
+            });
+        
+            return newPair.save((err, thisDoc) => {
+              if (err) return console.error(err);
+              res.json({
+                original_url: thisDoc.original_url,
+                short_url: thisDoc.short_url
+              });
+            });
+          });
+      }
+      
+      // Return JSON object for original_url already in DB
+      return res.json({
+        original_url: pair.original_url,
+        short_url: pair.short_url
+      });
+    });
+  });
+});
+
+// Redirect short_url to original_url
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  urlModel.findOne({short_url: req.params.shortUrl}, (err, data) => {
+    let shorty = data.original_url;
+    res.redirect(shorty);
+  });
 });
 
 
